bffd96d29b3ae67698714bb01b29736169edffe4
jones <maikoon.jones@gmail.com>
2023-03-29 22:59:07 -0300

    lp finalizada

--- script.js
+++ script.js
@@ -1,31 +1,26 @@
-const dia = document.getElementById('dia');
-const hora = document.getElementById('hora');
-const minuto = document.getElementById('minuto');
-const segundo = document.getElementById('segundo');
-
-const launch = "17 oct 2024";
-
-function countDown(){
-  const dataLaunch = new Date(launch)
-  const now = new Date.now()
-
-  const totalSeconds = (dataLaunch - now)/1000;
-
-  const days = Math.floor(totalSeconds / 60 / 60 / 24);
-
-  const hours = Math.floor(totalSeconds / 60 / 60) % 24;
-
-  const minutes = Math.floor(totalSeconds / 60) % 60;
-
-  const seconds = Math.floor(totalSeconds) % 60;
-
-  dia.innerHTML = days;
-  hora.innerHTML = hours;
-  minuto.innerHTML = minutes;
-  segundo.innerHTML = seconds;
-  
+const countdown = {
+  dia: document.getElementById('dia'),
+  hora: document.getElementById('hora'),
+  minuto: document.getElementById('minuto'),
+  segundo: document.getElementById('segundo'),
+  launch: new Date('2023-07-17T00:00:00'),
+  update() {
+    const now = new Date();
+    const totalSeconds = Math.floor((this.launch - now) / 1000);
+    const days = Math.floor(totalSeconds / 86400);
+    const hours = Math.floor(totalSeconds % 86400 / 3600);
+    const minutes = Math.floor(totalSeconds % 3600 / 60);
+    const seconds = Math.floor(totalSeconds % 60);
+    this.dia.innerHTML = days+'D';
+    this.hora.innerHTML = formatTime(hours)+'H';
+    this.minuto.innerHTML = formatTime(minutes)+'M';
+    this.segundo.innerHTML = formatTime(seconds)+'S';
+  }
 }
 
-countDown();
+function formatTime(time) {
+  return String(time).padStart(2, '0');
+}
 
-setInterval(countDown, 1000)
+countdown.update();
+setInterval(() => countdown.update(), 1000);
